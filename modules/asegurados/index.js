ecd5706abc70d4aec6f47fbb972a6d4ede655d16
guillermojmc <guillermojmc@gmail.com>
2014-07-21 17:30:05 -0400

    Inicio del modulo de asegurados

---
+++ modules/asegurados/index.js
@@ -0,0 +1,17 @@
+// var express = require('express'),
+//     router = module.exports = express.Router();
+//
+//
+// // Recurso Dogs
+// dogsHandler = require('./dogsHandler');
+// router.get('/', dogsHandler.lista); // listado
+// router.get('/new', dogsHandler.agregar.formulario); // formulario de agregar
+// router.post('/', dogsHandler.agregar.guardar); // procesa nuevo item
+//
+//
+// // Recurso Dogs/:id
+// dogsIdHandler = require('./dogsIdHandler');
+// router.get('/:id', dogsIdHandler.ver) // ver
+// router.get('/:id/edit', dogsIdHandler.editar.formulario) // editar formulario
+// router.put('/:id', dogsIdHandler.editar.guardar) // editar guardar
+// router.get('/:id/delete', dogsIdHandler.eliminar) // eliminar
