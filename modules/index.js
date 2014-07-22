36b0afdfe5a5f9fade68341c840ec6dfe71280d7
Toledo95 <ctoledo9672@gmail.com>
2014-07-22 18:06:20 -0400

    japiro

--- modules/index.js
+++ modules/index.js
@@ -2,11 +2,6 @@ var express = require('express'),
     router = module.exports = express.Router();
 
 router.use('/dogs', require('./dogs'));
-<<<<<<< Updated upstream
 router.use('/asegurados', require('./asegurados'));
 // router.use('/medicos', require('./medicos'));
-=======
-// router.use('/asegurados', require('./asegurados'));
-// router.use('/medicos', require('./medicos'));
 router.use('/insumos', require('./insumos'))
->>>>>>> Stashed changes
